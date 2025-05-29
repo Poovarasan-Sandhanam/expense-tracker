7f8279c87e083d7edcb5811de6a7ce828d43f1d1
Poovarasan-Sandhanam <poovarasansandhanam@gmail.com>
2025-05-29 23:38:08 +0100

    Expo setup deleted

--- frontend/App.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthProvider } from './src/context/AuthContext';
-
-export default function App() {
-  return <AuthProvider children={undefined} />;
-}
--- frontend/app/signup.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function SignUpScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
-      <Text>Sign Up Screen</Text>
-    </View>
-  );
-}
