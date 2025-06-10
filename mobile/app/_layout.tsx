fca2492b690f25dc8d1df6be4a3d99588a1c7455
Poovarasan-Sandhanam <poovarasansandhanam@gmail.com>
2025-06-10 21:00:46 +0100

    expo setup done with proper structured

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+import SafeScreen from "@/components/safeScreen";
+
+export default function RootLayout() {
+  return(
+    <SafeScreen>
+      <Stack screenOptions={{headerShown:false}}/>
+    </SafeScreen>
+  );
+}
